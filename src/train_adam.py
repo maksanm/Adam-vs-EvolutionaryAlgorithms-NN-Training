c3550835f42d0a79b55a9b038234f6243934eae0
ksulkowski <kamilsulkowskiwork@gmail.com>
2025-06-20 17:46:03 +0200

    Add adam metrics

--- src/train_adam.py
+++ src/train_adam.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 import torch
 import torch.nn as nn
@@ -7,7 +8,7 @@ from torch.utils.data import DataLoader
 
 from dataset import PeptideDataset
 from model import RetentionPredictor
-from utils import evaluate
+from utils import evaluate, evaluate_regression_metrics
 
 from dotenv import load_dotenv
 load_dotenv(override=True)
@@ -25,6 +26,15 @@ torch.manual_seed(int(os.getenv("RANDOM_SEED")))
 def train_adam(model, criterion, train_dataloader, val_dataloader):
     optimizer = optim.Adam(model.parameters(), lr=ADAM_LR)
 
+    learning_history = {
+        "epoch": [],
+        "train_loss": [],
+        "val_loss": [],
+        "eval_calls": 0,
+        "timestamp": []
+    }
+
+    start = time.time()
     for epoch in range(ADAM_EPOCHS):
         model.train()
         epoch_train_losses = []
@@ -37,7 +47,21 @@ def train_adam(model, criterion, train_dataloader, val_dataloader):
 
         train_loss = sum(epoch_train_losses) / len(epoch_train_losses)
         val_loss = evaluate(model, val_dataloader, criterion, 'cpu')
+
+        learning_history["epoch"].append(epoch + 1)
+        learning_history["train_loss"].append(train_loss)
+        learning_history["val_loss"].append(val_loss)
+        learning_history["eval_calls"] += 1
+        learning_history["timestamp"].append(time.time() - start)
+
         print(f'Epoch {epoch + 1:3d}/{ADAM_EPOCHS} | Train Loss: {train_loss:.5f} | Val Loss: {val_loss:.5f}')
+
+    metrics = evaluate_regression_metrics(model, val_dataloader)
+    learning_history["final_mse"] = metrics["mse"]
+    learning_history["final_mae"] = metrics["mae"]
+    learning_history["final_r2"] = metrics["r2"]
+
+    return model, learning_history
 
 
 if __name__ == "__main__":
@@ -69,4 +93,4 @@ if __name__ == "__main__":
 
     # =================== RUN EXPERIMENTS ===================
     print("Training with Adam:")
-    model = train_adam(model, criterion, train_dataloader, val_dataloader)
+    model, history = train_adam(model, criterion, train_dataloader, val_dataloader)
